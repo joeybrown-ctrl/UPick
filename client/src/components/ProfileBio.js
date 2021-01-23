b1c3d2ed7c68501cb36a78615e65588c7b9e8a86
Slogreco <shoshwright@hotmail.com>
2021-01-23 14:23:53 -0700

    created picks page. reformatted profile page and created separate components for profile page

---
+++ client/src/components/ProfileBio.js
@@ -0,0 +1,20 @@
+import { Card } from 'react-bootstrap';
+
+const styles = {
+
+    text: {
+        color: '#c5c1c0'
+    }
+};
+
+const ProfileBio = () => {
+    return (
+        <Card.Text style={styles.text}>
+            Profile Bio Goes Here
+        </Card.Text>
+    );
+};
+
+export default ProfileBio;
+
+
