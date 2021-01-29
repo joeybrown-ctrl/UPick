77654bfcabf1c6575fff34c1bf24ae32e534daf2
Joey Brown <gjoey.brown@gmail.com>
2021-01-28 19:41:56 -0800

    finished routing in usersController and eventController

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -4,7 +4,7 @@ const isAuthenticated = require('../utils/middleware').isAuthenticated;
 /**
  * Vote - Read All
  */
-router.get('/', isAuthenticated, function(req, res) {
+router.get('/', isAuthenticated, function (req, res) {
     // we can pass in things in the query of a REST call!
     db.Vote.findAll(req.query)
         .then(dbModel => res.json(dbModel))
@@ -14,8 +14,21 @@ router.get('/', isAuthenticated, function(req, res) {
 /**
  * Vote - Read One
  */
-router.get('/:id', isAuthenticated, function(req, res) {
+router.get('/:id', isAuthenticated, function (req, res) {
     db.Vote.findByPk(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+});
+
+// * Note - Create
+// * Notice how we are also taking in the User Id! Important!
+// * We need the isAuthenticated middleware in the route to have a user in the request
+// */
+router.post('/', isAuthenticated, function (req, res) {
+    db.Vote.create({
+        UserId: req.user.id,
+        ...req.body
+    })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
 });
@@ -23,6 +36,5 @@ router.get('/:id', isAuthenticated, function(req, res) {
 //create vote (POST route)
 //add logic to determine whether or not event is completed (join votes to activities onto events)
 //create algo to find out which activity user picked
-
 
 module.exports = router;
