73f0fd2a1f1609671e54dc7197da85860eefdbf7
Joey Brown <gjoey.brown@gmail.com>
2021-02-23 13:17:27 -0800

    moved vote functionality from UPicked.js to UPickedCard.js, figuring out how to filter data within component.

--- client/src/pages/UPicked.js
+++ client/src/pages/UPicked.js
@@ -1,44 +1,40 @@
 import UPickedCard from '../components/UPickedCard';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
 
 // this page will be where the user is redirected after voting
 // get activities for event, including votes
 
 function YouPicked() {
-    const [results, setResults] = useState([]);
-    const { eventId } = useParams();
-    useEffect(() => {
-        setInterval(() => fetchVotes(),10000);
-    },[]);
-    function fetchVotes() {
-        axios.get(`/api/activity/${eventId}`).then(({ data }) => {
-            const transformedVotes = data.map(activity => {
-                const total = {
-                    yes: 0,
-                    no: 0
-                };
-                activity?.Votes.forEach(vote => {
-                    if (vote.status) {
-                        total.yes++;
-                    } else if (!vote.status) {
-                        total.no++;
-                    }
-                });
-                return {
-                    total,
-                    Name: activity.Name,
-                    URL: activity.URL
-                };
-            });
-            setResults(transformedVotes);
-        });
-    }
-  
+    // const [results, setResults] = useState([]);
+    // const { eventId } = useParams();
+    // useEffect(() => {
+    //     setInterval(() => fetchVotes(),10000);
+    // },[]);
+    // function fetchVotes() {
+    //     axios.get(`/api/activity/${eventId}`).then(({ data }) => {
+    //         const transformedVotes = data.map(activity => {
+    //             const total = {
+    //                 yes: 0,
+    //                 no: 0
+    //             };
+    //             activity?.Votes.forEach(vote => {
+    //                 if (vote.status) {
+    //                     total.yes++;
+    //                 } else if (!vote.status) {
+    //                     total.no++;
+    //                 }
+    //             });
+    //             return {
+    //                 total,
+    //                 Name: activity.Name,
+    //                 URL: activity.URL
+    //             };
+    //         });
+    //         setResults(transformedVotes);
+    //     });
+    // }
+
     return(
         <>
-            {JSON.stringify(results)}
             <UPickedCard/>
         </>
     );
