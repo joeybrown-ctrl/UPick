0c03a6175aee73edeb21d6c4caa2045bbb43c9eb
Slogreco <shoshwright@hotmail.com>
2021-02-25 13:33:48 -0700

    moved image stuff into a controller from server

--- server.js
+++ server.js
@@ -60,21 +60,4 @@ db.sequelize.sync({ force: false }).then(function () {
     });
 });
 
-//Sending photos to cloudinary storage
-cloudinary.config({
-    CLOUD_NAME: process.env.CLOUD_NAME,
-    CLOUD_APIIKEY: process.env.API_KEY,
-    CLOUD_API_SECRET: process.env.API_SECRET
-});
 
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}));
-
-app.use(formData.parse());
-
-app.post('/image-upload-single', (req, res) => {
-    const path = Object.values(Object.values(req.files)[0])[0].path;
-    cloudinary.uploader.upload(path)
-        .then(image => res.json([image]));
-});
